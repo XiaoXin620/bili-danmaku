49088c257f4f4e8e73e3c51de88375d57b3a164f
mason <786019030@qq.com>
2023-04-25 12:15:44 +0800

    update package

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 // import "./style.css"
+import ArcoVue from '@arco-design/web-vue';
+// 额外引入图标库
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import App from './App.vue'
-// import './samples/node-api'
+import './samples/node-api'
+import '@arco-design/web-vue/dist/arco.css';
 import router from './router'
 
 createApp(App)
   .use(router)
+  .use(ArcoVue)
+  .use(ArcoVueIcon)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
